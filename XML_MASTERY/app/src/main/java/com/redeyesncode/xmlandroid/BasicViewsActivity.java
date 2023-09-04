7cc9a9503bbe0e731876ba929a859ed2c1b046e4
RedEyesNCode <ashutosh.singh19071999@gmail.com>
2023-09-04 13:48:30 +0530

    Push basic views code

---
+++ XML_MASTERY/app/src/main/java/com/redeyesncode/xmlandroid/BasicViewsActivity.java
@@ -0,0 +1,14 @@
+package com.redeyesncode.xmlandroid;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class BasicViewsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_basic_views);
+    }
+}
